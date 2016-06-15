67baaa90c2be75abb3a421e0ac4bde6d6891e3ae
qiuzz <qiuzz@getui.com>
2016-06-15 18:19:24 +0800

    展示图片

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,16 @@
 /**
  * Created by apple on 16/6/15.
  */
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs = require("fs");
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>'+
         '<head>'+
-        '<meta http-equiv="Content-Type" content="text/html; '+
-        'charset=UTF-8" />'+
+        '<meta http-equiv="Content-Type" '+
+        'content="text/html; charset=UTF-8" />'+
         '</head>'+
         '<body>'+
         '<form action="/upload" method="post">'+
@@ -32,5 +33,21 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response, postData) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("./test.jpg", "binary", function(error, file) {
+        if(error) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
